7258ab17fda8e316228ec674615ce8ce26e403da
Matthew Yeung <matthewyeung34@gmail.com>
2022-02-07 15:37:42 -0500

    b4 change

--- testing.py
+++ testing.py
@@ -4,33 +4,17 @@ from openpyxl import load_workbook
 
 import time
 # generate city and region for page3
-def city_comment_gen():
-    result = ""
-    if city.title() == "Ottawa":
-        result = "City of Ottawa;"
-    elif city.title() == "Toronto":
-        result = "City of Toronto;"
-    elif city.title() == "Brampton":
-        result = "Region of Peel; City of Brampton"
-    elif city.title() == "Mississauga":
-        result = "Region of Peel; City of Missisauga"
-    elif city.title() == "Pickering":
-        result = "Region of Durham; City of Missisauga"
-    elif city.title() == "Oshawa":
-        result = "Region of Durham; Town of Oshawa"
-    elif city.title() == "Whitby":
-        result = "Region of Durham; Town of Whitby"
-    elif city.title() == "Ajax":
-        result = "Region of Durham; Town of Ajax"
-    elif city.title() == "Markham":
-        result = "Region of York; City of Markham"
-    elif city.title() == "Vaughan":
-        result = "Region of York; City of Vaughan"
-    return result
+def testing():
+    new = string.split(" Transfer ")
+    print(new)
+    new2 = new[0].split(" $")
+    print(new2)
+    money = new2[1].replace(",",'')
+    print(money)
 def main():
-    global city
-    city = "brampton"
-    print (city_comment_gen())
+    global string
+    string = "Dec 15, 2017 $625,253 Transfer BHAT, POONAM SUSHIL; BHAT, SUSHILKUMAR PRI;"
+    testing()
 
 
 
